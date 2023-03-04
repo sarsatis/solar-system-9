6e4b55ca57de90b3610ac12789a53fb7a7d1248b
sarsatis <sarthak8055@gmail.com>
2023-03-04 21:19:22 +0000

    updated pod template

--- pr.sh
+++ pr.sh
@@ -1,10 +1,26 @@
 echo "Opening a Pull Request"
 
-curl -X 'POST' \
-  'https://github.com/sarsatis/gitops-argocd/pulls' \
-  -H 'accept: application/json' \
-  -H "authorization: $GITHUB_TOKEN" \
-  -H 'Content-Type: application/json' \
+# curl -X 'POST' \
+#   'https://github.com/sarsatis/gitops-argocd/pulls' \
+#   -H 'accept: application/json' \
+#   -H "authorization: $GITHUB_TOKEN" \
+#   -H 'Content-Type: application/json' \
+#   -d '{
+#   "assignee": "sarsatis",
+#   "assignees": [
+#     "sarsatis"
+#   ],
+#   "base": "main",
+#   "body": "Updated deployment specification with a new image version.",
+#   "head": "feature-test",
+#   "title": "Updated Solar System Image"
+# }'
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  'https://api.github.com/repos/sarsatis/gitops-argocd/pulls' \
   -d '{
   "assignee": "sarsatis",
   "assignees": [
@@ -17,3 +33,4 @@ curl -X 'POST' \
 }'
 
 echo "Success"
+
