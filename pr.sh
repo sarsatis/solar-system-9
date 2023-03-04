8ca3dfabd2a16ea88da14d159b894babdd75c5ae
sarsatis <sarthak8055@gmail.com>
2023-03-04 21:50:14 +0000

    updated pod template

--- pr.sh
+++ pr.sh
@@ -16,14 +16,12 @@ echo "Opening a Pull Request"
 #   "title": "Updated Solar System Image"
 # }'
 
-echo "sa ${GITHUB_TOKEN}"
-githupass = URLEncoder.encode("${GITHUB_TOKEN}",'UTF-8')
-echo "sa ${githupass}"
+
 
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${githupass}"\
+  -H "Authorization: Bearer ${GITHUB_TOKEN}"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
   'https://api.github.com/repos/sarsatis/gitops-argocd/pulls' \
   -d '{
