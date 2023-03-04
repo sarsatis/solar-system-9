e58f0c180b7da050e6fe4180f8167400bac594d4
sarsatis <sarthak8055@gmail.com>
2023-03-04 22:25:41 +0000

    updated pod template

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ echo '<!DOCTYPE html>
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <style>
 		#solar-system {
-		   background: url("images/solar-system-9.png")   center center;
+		   background: url("images/solar-system-6.png")   center center;
            background-repeat: no-repeat;			  
 		   background-size: cover;
 		   content: "";
